3fdb05bd6fc4104fe116989d9a2ac318a50dd445
vichule <javier.cabanasdiaz@gmail.com>
2024-05-15 16:14:32 +0200

    footer styled and updated

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components"
 
 
 export const Footer = () => {
@@ -5,8 +6,16 @@ export const Footer = () => {
     return(
         <>
             <footer>
-                <h3>my footer actually</h3>
+                <FooterText>© Copyright 2024 | Javier Cabañas</FooterText>
             </footer>
         </>
     )
 }
+
+const FooterText = styled.h3`
+    text-align: center;
+    margin: 0;
+    padding: 1em;
+    background-color: black;
+    letter-spacing: 3px;
+`
